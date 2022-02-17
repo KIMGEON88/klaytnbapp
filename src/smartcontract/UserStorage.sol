45c484bfdd56d3a288e554c24fefc149190b5d68
KIMGEON88 <geonking88@gmail.com>
2022-02-17 16:37:23 +0900

    UserStorage

---
+++ src/smartcontract/UserStorage.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.5.6;
+
+import "filename";
+
+contract UserStorage{
+    mapping (address => uint) userData; // 상태변수
+
+    function set(uint x)public{
+        userData[msg.sender] = x;
+    }
+    function get()public view returns(uint){
+        return userData[msg.sender];
+    }
+    function getUserData(address user)public view returns(uint){
+        return userData[user];
+    }   
+    // View일 경우 상태 변수는 수정 안 됨.
+}
